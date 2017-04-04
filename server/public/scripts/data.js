05d46fe433fd440fcdd7bb9605ff641d53a66539
Huck Brock <huckbrock@gmail.com>
2017-04-04 13:06:44 -0500

    data

---
+++ server/public/scripts/data.js
@@ -0,0 +1,21 @@
+var peopleArray = [
+  {name: "Anisa Abdulkadir", shoutout: "Anisa showed me up with her Atom packages. Now its time for some retaliation.", github: "aanisa"},
+  {name: "Anna Springfield", shoutout: "Thanks to Anna for continuing to be a resource for asking tough questions, and for having good conversations.", github: "aespringfield"},
+  {name: "Chris Keller", shoutout: "Thanks to Chris for his speech today. I appreciated him talking about his family growing up and sharing. ", github: "CJamesKeller"},
+  {name: "Claudia Calderas Mendez", shoutout: "I had great comfort and support when working with Claudia today.", github: "claudiacalderas"},
+  {name: "Craig Baird", shoutout: "Craig's vulnerability in his speech today made me feel less alone with my own negative self talk.", github: "craigbaird"},
+  {name: "Betsy Rowley", shoutout: "Shout out to Betsy for her help on the challenge. She was a lot of help and I appreciate her as a team mate.", github: "BetsyRowley"},
+  {name: "Emily Hoang", shoutout: "Emily gave a brave speech today, thank you for sharing your story with us.", github: "EmilyHHLam"},
+  {name: "Erin Kinnen", shoutout: "Shoutout to Erin for telling hilarious stories about bats", github: "erinkinnen"},
+  {name: "Keith Tomlinson", shoutout: "Shoutout to Keith for his speech today. It was brave for a lot of reasons. Additionally I appreciate his thoughtfulness. ", github: "KBarrettTomlinson"},
+  {name: "Kevin Dahlberg", shoutout: "Thanks to Kevin for his speech today, I can relate to him and I am not sure I would have gotten to know that side of him if he did not share.", github: "KevinDahlberg"},
+  {name: "Lisa Schoofs", shoutout: "Lisa, for being both super outgoing and organized enough to productively channel all that socializing!", github: "lisaschoofs"},
+  {name: "Logan Kelly", shoutout: "Thanks to Logan for his role as grammarian, I thought he was very empathetic and compassionate while still being able to give good constructive criticisms.", github: "gnargnor"},
+  {name: "Nic Wilson", shoutout: "I want to thank Nic for how simply and concisely he understands and explains what is happening in the code. I really helped solidify some key concepts today. Thank you.", github: "nicolaslwilson"},
+  {name: "Olga Engels", shoutout: "Shoutout to Olga for working hard to be able to explain the steps in our group assignment!", github: "oengels01"},
+  {name: "Teigen Leonard", shoutout: "Shout out to Teigen for sticking with me for the long haul and making some good progress together.", github: "teigenleonard"},
+  {name: "Y Paul Sussman", shoutout: "Y Paul's outgoing nature and positive attitude has made me feel more comfortable and welcome here at Prime.", github: "ypaulsussman"},
+  {name: "Brianna Dickman", shoutout: "Shout out to Bri for noticing that I wasn't tracking and offering to go over the stuff again. She clarified things for me and I was able to move forward.", github: "briannadickman"},
+  {name: "Dan Zera", shoutout: "Dan because he has a great collaborative attitude and is very helpful.", github: "danzera"}
+
+];
